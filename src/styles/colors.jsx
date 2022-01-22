f66d41b689e8ee3fde83dfc69f03392439d329a9
Benny Jou <a33068843@gmail.com>
2022-01-22 22:58:28 +0800

    feat: add color palette

---
+++ src/styles/colors.jsx
@@ -0,0 +1,26 @@
+const primary400 = '#F6D365';
+const primary500 = '#86743A';
+const secondary400 = '#FDA085';
+const gray400 = '#282828'
+const gray500 = '#141414'
+
+
+const primary = primary400;
+const secondary = secondary400;
+const title = '#FFFFFF';
+const subtitle = '#B6B6B9';
+const white = '#FFFFFF';
+const black = '#000000';
+const background = '#161616'
+export const colors = {
+  primary500,
+  gray400,
+  gray500,
+  primary,
+  secondary,
+  title,
+  subtitle,
+  white,
+  black,
+  background
+}
