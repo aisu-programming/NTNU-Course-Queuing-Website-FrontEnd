03b81d24822458ccf5cc312860130e98c0699056
Benny Jou <a33068843@gmail.com>
2022-01-23 03:32:02 +0800

    style: adjust global colors with gray palette

--- src/styles/colors.jsx
+++ src/styles/colors.jsx
@@ -1,19 +1,26 @@
 const primary400 = '#F6D365';
 const primary500 = '#86743A';
 const secondary400 = '#FDA085';
+const gray100 = '#FFFFFF'
+const gray200 = '#B6B6B9'
+const gray300 = '#797A7F'
 const gray400 = '#282828'
 const gray500 = '#141414'
+const gray900 = '#000000'
 
 
 const primary = primary400;
 const secondary = secondary400;
-const title = '#FFFFFF';
-const subtitle = '#B6B6B9';
-const white = '#FFFFFF';
-const black = '#000000';
+const title = gray100;
+const subtitle = gray200;
+const white = gray100;
+const black = gray900;
 const background = '#161616'
 export const colors = {
   primary500,
+  gray100,
+  gray200,
+  gray300,
   gray400,
   gray500,
   primary,
