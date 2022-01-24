df294ab485690768c9738ff07498e8fe3ea04903
danny-git1005 <danny871005@gmail.com>
2022-01-24 23:10:17 +0800

    feat: post function complete

--- src/api/Post.jsx
+++ src/api/Post.jsx
@@ -1,18 +1,16 @@
+import { config } from "./config";
 
 export const PostApi = async(props) => { 
-  
-  console.log('123');
 
-  await fetch(
-    'http://192.168.1.109:5000/auth/session',{
+  return await fetch(
+    config.url,{
       method: 'POST',
       headers:{
         'Content-Type': 'application/json'
       },
       body:JSON.stringify(props)
     })
-    // .then((response) => response )
-    .finally((response) => response)
+    .then(response => response.json()  )
+    .catch(error => console.log(error) );    // .catch((error) => console.log(error))
 
 }
- 
