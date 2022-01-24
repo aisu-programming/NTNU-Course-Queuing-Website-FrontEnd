0d0845aeca959e5d7a38e955f2a2cfb0e7536cf9
Benny Jou <a33068843@gmail.com>
2022-01-24 16:23:20 +0800

    fix: adjust global colors

--- src/styles/colors.jsx
+++ src/styles/colors.jsx
@@ -1,33 +1,57 @@
-const primary400 = '#F6D365';
-const primary500 = '#86743A';
-const secondary400 = '#FDA085';
-const gray100 = '#FFFFFF'
-const gray200 = '#B6B6B9'
-const gray300 = '#797A7F'
-const gray400 = '#282828'
-const gray500 = '#141414'
-const gray900 = '#000000'
+const primary400 = "#F6D365";
+const primary500 = "#86743A";
+const secondary400 = "#FDA085";
+const gray000 = "#FFFFFF";
+const gray100 = "#D0D0D3";
+const gray200 = "#B6B6B9";
+const gray300 = "#797A7F";
+const gray400 = "#282828";
+const gray500 = "#1E1E1E";
+const gray600 = "#161616";
+const gray700 = "#141414";
+const gray900 = "#000000";
 
+const opacity10 = "19";
+const opacity20 = "33";
+const opacity30 = "4C";
+const opacity40 = "66";
+const opacity50 = "7F";
+const opacity60 = "99";
+const opacity70 = "B2";
+const opacity80 = "CC";
+const opacity90 = "E5";
 
 const primary = primary400;
 const secondary = secondary400;
-const title = gray100;
+const title = gray000;
 const subtitle = gray200;
-const white = gray100;
+const white = gray000;
 const black = gray900;
-const background = '#161616'
+const background = gray600;
 export const colors = {
   primary500,
+  gray000,
   gray100,
   gray200,
   gray300,
   gray400,
   gray500,
+  gray600,
+  gray700,
+  opacity10,
+  opacity20,
+  opacity30,
+  opacity40,
+  opacity50,
+  opacity60,
+  opacity70,
+  opacity80,
+  opacity90,
   primary,
   secondary,
   title,
   subtitle,
   white,
   black,
-  background
-}
+  background,
+};
