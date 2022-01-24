9e78cb3cc22c51fe7d64c9048ce44a605a21e679
danny-git1005 <danny871005@gmail.com>
2022-01-24 23:10:54 +0800

    feat: get function complete

--- src/api/Get.jsx
+++ src/api/Get.jsx
@@ -1,15 +1,17 @@
+import { config } from "./config"
+
 export const GetApi = async(props) => { 
   
-    await fetch(
-      'http://192.168.1.109:5000/auth/session',{
-        method: 'GET',
-        headers:{
-          'Content-Type': 'application/json'
-        },
-        body:JSON.stringify(props)
-      })
-      .then((response) => console.log(response,))
-      .catch((error) => console.log('error'+error))
+  return await fetch(
+    config.url,{
+      method: 'GET',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(props)
+    })
+    .then((response) => response.json())
+    .catch((error) => console.log('error'+error))
   
 }
   
