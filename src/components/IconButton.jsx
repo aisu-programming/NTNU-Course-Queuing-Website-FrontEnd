0f9f4b20be60686ecbc1ef817a802888f3953632
Benny Jou <a33068843@gmail.com>
2022-02-11 12:50:37 +0800

    feat: add iconbutton component

---
+++ src/components/IconButton.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../styles';
+
+const Button = styled.div`
+  width: fit-content;
+  color: ${(props) =>
+    props.isDanger ? props.isDanger : colors.gray600};
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  padding: 8px 10px;
+  border-radius: 8px;
+  cursor: pointer;
+  border: 1px solid ${colors.white}${colors.opacity50};
+  background: ${colors.white}${colors.opacity40};
+  box-shadow: 2px 2px 8px ${colors.black}${colors.opacity20};
+  transition: 0.3s;
+
+  svg {
+    transform: '';
+    transition: .3s;
+  }
+  &:hover {
+    background: ${colors.white}${colors.opacity60};
+    svg {
+      transform: rotate(10deg);
+    }
+  }
+
+  svg {
+    height: 16px;
+    width: 16px;
+    margin-right: 4px;
+  }
+`;
+
+const Text = styled.h4`
+  position: relative;
+  top: 1px;
+`;
+
+export const IconButton = ({
+  isDanger='',
+  text,
+  children,
+}) => {
+  console.log(!!isDanger)
+  return (
+    <Button isDanger={isDanger}>
+      {children}
+      <Text>{text}</Text>
+    </Button>
+  );
+};
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export { Navigation } from './Navigation';
 export { ButtonOption } from './ButtonOption';
+export { IconButton } from './IconButton';
 export { Table, Test } from './Table';
