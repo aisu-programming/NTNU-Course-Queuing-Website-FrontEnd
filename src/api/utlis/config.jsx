93ca9df5e75bfaffbb2793a856631c8e22e9b3d3
Benny Jou <a33068843@gmail.com>
2022-02-13 12:18:03 +0800

    fix: change backend port

--- src/api/utlis/config.jsx
+++ src/api/utlis/config.jsx
@@ -1,7 +1,7 @@
 export const config = {
-    loginUrl: "http://114.24.81.44:12345/auth/session",
-    searchUrl:"http://114.24.81.44:12345/course/search",
-    listUrl:"http://114.24.81.44:12345/order",
-    achieveUrl:"http://114.24.81.44:12345/order/achievement",
+    loginUrl: "http://114.24.81.44:5000/auth/session",
+    searchUrl:"http://114.24.81.44:5000/course/search",
+    listUrl:"http://114.24.81.44:5000/order",
+    achieveUrl:"http://114.24.81.44:5000/order/achievement",
 
 }
