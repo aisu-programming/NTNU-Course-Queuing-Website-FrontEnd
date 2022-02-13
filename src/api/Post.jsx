52bf1828a7b7f5176e4349fab3bfdf2d06686e99
Benny Jou <a33068843@gmail.com>
2022-02-13 18:18:37 +0800

    fix: adjust post api header

--- src/api/Post.jsx
+++ src/api/Post.jsx
@@ -1,14 +1,14 @@
-export const PostApi = async(props , url) => { 
-
-  return await fetch(
-    url,{
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify(props)
-    })
-    .then(response => response.json()  )
-    .catch(error => console.log(error) );    // .catch((error) => console.log(error))
-
-}
+export const PostApi = async (props, url) => {
+  return await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Credentials': true,
+    },
+    mode: 'cors',
+    credentials: 'include',
+    body: JSON.stringify(props),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log(error)); // .catch((error) => console.log(error))
+};
