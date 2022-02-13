03785d311af7d865a96754fa9776b38b9685021f
Benny Jou <a33068843@gmail.com>
2022-02-13 18:14:10 +0800

    fix: add overbody background color

--- src/styles/globalStyle.jsx
+++ src/styles/globalStyle.jsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const ResetStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video {
@@ -32,7 +32,8 @@ html {
   box-sizing: border-box;
 }
 body {
-    line-height: 1;
+  background: #161616;
+  line-height: 1;
 }
 ol, ul {
   list-style: none;
@@ -85,4 +86,4 @@ export const GlobalStyle = createGlobalStyle`
   div {
     font-family: 'Noto Sans TC', sans-serif;
   }
-`
+`;
